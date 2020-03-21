1a50bd51d545b421a0114f8e278a1b2ea29c54a8
ohlillyo <lily.ong125@gmail.com>
2020-03-21 16:35:09 -0700

    bot types, task descriptions...
    initial bot constructor

---
+++ script.js
@@ -0,0 +1,82 @@
+const botTypes = {
+  'Unipedal': {
+    maxPoints: false,
+    maxTasks: false,
+    friendly: true,
+  },
+  'Bipedal': {
+    maxPoints: false,
+    maxTasks: true,
+    friendly: true,
+  },
+  'Quadrupedal': {
+    maxPoints: true,
+    maxTasks: false,
+    friendly: true,
+  },
+  'Aeronautical': {
+    maxPoints: true,
+    maxTasks: true,
+    friendly: true,
+  },
+  'Radial': {
+    maxPoints: false,
+    maxTasks: true,
+    friendly: false,
+  },
+  'Arachnid': {
+    maxPoints: true,
+    maxTasks: true,
+    friendly: false,
+};
+
+const tasks =
+[
+  {
+    description: 'do the dishes',
+    eta: 1000,
+    points: 15,
+  },{
+    description: 'sweep the house',
+    eta: 3000,
+    points: 20,
+  },{
+    description: 'do the laundry',
+    eta: 10000,
+    points: 15,
+  },{
+    description: 'take out the recycling',
+    eta: 4000,
+    points: 10,
+  },{
+    description: 'make a sammich',
+    eta: 7000,
+    points: 10,
+  },{
+    description: 'mow the lawn',
+    eta: 20000,
+    points: 30,
+  },{
+    description: 'rake the leaves',
+    eta: 18000,
+    points: 30,
+  },{
+    description: 'give the dog a bath',
+    eta: 14500,
+  },{
+    description: 'bake some cookies',
+    eta: 8000,
+    points: 20,
+  },{
+    description: 'wash the car',
+    eta: 20000,
+    points: 25,
+  },
+];
+
+
+function newBot(name, model) {
+  this.name = name;
+  this.model = model;
+  this.stats = botTypes[model]
+}
